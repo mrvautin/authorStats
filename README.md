9f38c6853e2c66c40be0fb73013719e5fd1a5148
Mark Moffat <hi@markmoffat.com>
2018-04-08 17:02:30 +0200

    Fixed up all issues.

---
+++ README.md
@@ -0,0 +1,21 @@
+# authorStats
+
+Fetch the daily/weekly/monthly download stats for all your NPM packages/modules and print to a pretty unicode table.
+
+## Installation
+
+```
+# npm install author-stats -g
+```
+
+## Usage
+
+```
+# authorStats <npm username>
+```
+
+Where `<npm username>` is the username on the NPM website. My profile is: `https://www.npmjs.com/~mrvautin` and username is `mrvautin`.
+
+A nice command line table with the daily, weekly and monthly download numbers of all your packages will be output to your terminal.
+
+![authorStats](https://raw.githubusercontent.com/mrvautin/mrvautin.github.io/master/images/authorStats/exampleoutput.png "authorStats output")
---
+++ index.js
@@ -0,0 +1,154 @@
+#!/usr/bin/env node
+
+const commander = require('commander');
+const async = require('async');
+const cliTable = require('cli-table');
+const cliSpinner = require('cli-spinner').Spinner;
+const registry = require('npm-registry');
+const colors = require('colors');
+const _ = require('lodash');
+const pkg = require('./package.json');
+
+// get the package version from the package.json file
+const version = pkg.version;
+
+// setup commander
+commander
+    .version(version)
+    .usage('<username>')
+    .parse(process.argv);
+
+// get the author arg
+let author = commander.args;
+
+// check author arg has been specified and throw error
+if(author === ''){
+    commander.outputHelp(make_red);
+    process.exit(1);
+}
+
+// build the output table headers
+let table = new cliTable({
+    head: [
+        colors.cyan('Package name'),
+        colors.cyan('Daily Downloads'),
+        colors.cyan('Weekly Downloads'),
+        colors.cyan('Monthly Downloads'),
+        colors.cyan('Dependants')
+    ]
+});
+
+// start a new spinner
+const spinner = new cliSpinner('%s');
+spinner.setSpinnerString('|/-\\');
+spinner.start();
+
+const npm = new registry({
+    registry: 'https://registry.npmjs.org'
+});
+
+npm.users.list(author.toString(), (err, userPackages) => {
+    if(err || userPackages.length === 0){
+        console.log('\n');
+        console.error(colors.red('ERROR: Author not found or no packages. Check author username and try again.'));
+        spinner.stop(true);
+        process.exit(1);
+    }
+
+    const tableObject = [];
+
+    async.each(userPackages, (pkg, callback) => {
+        Promise.all([
+            getDownloads('last-day', pkg.name),
+            getDownloads('last-week', pkg.name),
+            getDownloads('last-month', pkg.name),
+            getDependants(pkg.name)
+        ])
+        .then(([lastDay, lastWeek, lastMonth, dependants]) => {
+            tableObject.push(
+                {
+                    name: pkg.name,
+                    lastDay: lastDay[0].downloads,
+                    lastWeek: lastWeek[0].downloads,
+                    lastMonth: lastMonth[0].downloads,
+                    dependants: dependants.length || 0
+                }
+            );
+            callback(null);
+        })
+        .catch((err) => {
+            console.log('\n');
+            console.error(colors.red('ERROR: Could not fetch packages. Please try again.'));
+            spinner.stop(true);
+            process.exit(1);
+        });
+    }, (err) => {
+        if(err){
+            // output the error
+            console.log('\n');
+            spinner.stop();
+            console.error(colors.red('ERROR: ' + err));
+            process.exit(1);
+        }
+
+        // output the table and stop the spinner
+        const sortedPackages = _.sortBy(tableObject, 'lastMonth').reverse();
+        console.log('\n');
+        let totalDay = 0;
+        let totalWeek = 0;
+        let totalMonth = 0;
+        let totalDependants = 0;
+        sortedPackages.forEach((pkg) => {
+            table.push([
+                colors.red(pkg.name),
+                colors.yellow(pkg.lastDay),
+                colors.yellow(pkg.lastWeek),
+                colors.yellow(pkg.lastMonth),
+                colors.yellow(pkg.dependants)
+            ]);
+            totalDay += pkg.lastDay;
+            totalWeek += pkg.lastWeek;
+            totalMonth += pkg.lastMonth;
+            totalDependants += pkg.dependants;
+        });
+
+        // Push totals
+        table.push([
+            colors.green('Totals'),
+            colors.green(totalDay),
+            colors.green(totalWeek),
+            colors.green(totalMonth),
+            colors.green(totalDependants)
+        ]);
+
+        console.log(table.toString().green);
+        spinner.stop();
+    });
+});
+
+// make the help text red
+function make_red(txt){
+    return colors.red(txt); // display the help text in red on the console
+}
+
+function getDownloads(period, pkg){
+    return new Promise((resolve, reject) => {
+        npm.downloads.totals(period, pkg, (err, data) => {
+            if(err){
+                reject(err);
+            }
+            resolve(data);
+        });
+    });
+};
+
+function getDependants(pkg){
+    return new Promise((resolve, reject) => {
+        npm.packages.depended(pkg, (err, data) => {
+            if(err){
+                reject(err);
+            }
+            resolve(data);
+        });
+    });
+};
